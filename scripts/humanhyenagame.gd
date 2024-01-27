464630a19963a2d5e8d96cf153bcca5a13457580
wolcade <wolcade@gmail.com>
2024-01-27 17:48:00 -0600

    player loading fix

--- scripts/humanhyenagame.gd
+++ scripts/humanhyenagame.gd
@@ -2,13 +2,13 @@ extends Node
 
 const SPAWN_RANDOM := 5.0
 @export var CharacterPath : String
+var readyCounter = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if not multiplayer.is_server():
+		signal_player_connected.rpc()
 		return
-	
-	for id in multiplayer.get_peers():
-		add_player(id)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -22,3 +22,10 @@ func add_player(id: int):
 	character.position = Vector3(pos.x * SPAWN_RANDOM * randf(), 0, pos.y * SPAWN_RANDOM * randf())
 	character.name = str(id)
 	$PlayerNode.add_child(character, true)
+
+@rpc("authority", "call_remote", "reliable")
+func signal_player_connected():
+	readyCounter += 1
+	if readyCounter == multiplayer.get_peers():
+		for id in multiplayer.get_peers():
+			add_player(id)
