9ad39a16899d7018fb735408dc111d0c3658f4e7
wolcade <wolcade@gmail.com>
2024-01-27 14:40:00 -0600

    scene switch test

--- scenes/UI.gd
+++ scenes/UI.gd
@@ -1,6 +1,7 @@
 extends Control
 const PORT = 6968
 const MAX_CLIENTS = 4
+const SCENE_FILEPATH = "res://scenes/humanhyenagame.tscn"
 
 signal player_connected(peer_id)
 signal player_disconnected(peer_id)
@@ -8,6 +9,7 @@ signal player_disconnected(peer_id)
 @export var IpAddrTextEdit : TextEdit
 @export var StartGameButton : Button
 @export var PlayerConnectedLabel : Label
+@export var GetPeersLabel : Label
 
 var playersLoaded = 0
 
@@ -19,9 +21,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(multiplayer.is_server) :
-		pass
-	pass
+	if(multiplayer.is_server()) :
+		GetPeersLabel.text = "Get peers: " + str(multiplayer.get_peers())
+		if(multiplayer.get_peers().size() > 1):
+			StartGameButton.disabled = false
 	
 func beginHosting():
 	var peer = ENetMultiplayerPeer.new()
@@ -40,13 +43,19 @@ func connectClient():
 func startGame():
 	pass
 
-func onPlayerConnect():
+func onPlayerConnect(peer):
 	playerConnected.rpc()
 	
 @rpc("any_peer", "call_local", "reliable")
 func playerConnected():
-	if(multiplayer.is_server):
+	if(multiplayer.is_server()):
 		playersLoaded += 1
 		PlayerConnectedLabel.text = "Players connected: " + str(playersLoaded)
 	print("New player connected")
 	
+@rpc("any_peer", "call_local", "reliable")
+func loadLevel():
+	get_tree().change_scene_to_file(SCENE_FILEPATH)
+	
+func callLoadLevel():
+	loadLevel.rpc()
