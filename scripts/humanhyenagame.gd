6a1883867da3e080e62b144b8853f9782827897f
wolcade <wolcade@gmail.com>
2024-01-27 17:10:45 -0600

    fixing rotations

--- scripts/humanhyenagame.gd
+++ scripts/humanhyenagame.gd
@@ -7,22 +7,15 @@ func _ready():
 	if not multiplayer.is_server():
 		return
 	
-	multiplayer.peer_connected.connect(add_player)
-	
 	for id in multiplayer.get_peers():
 		add_player(id)
-		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
-func _exit_tree():
-	if not multiplayer.is_server():
-		return
-	multiplayer.peer_connected.disconnect(add_player)
 
 func add_player(id: int):
+	print("adding player" + str(id))
 	var character = preload("res://prefabs/player_character.tscn").instantiate()
 	character.player = id
 	var pos := Vector2.from_angle(randf() * 2 * PI)
