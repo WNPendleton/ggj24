88648df4f8ce5451d41e1edfcc048cdfd9bd4d2e
WNPendleton <WNPendleton@gmail.com>
2024-01-27 20:41:50 -0600

    Approach state

--- scripts/hyena.gd
+++ scripts/hyena.gd
@@ -13,6 +13,7 @@ var my_state = state.WANDER
 var player_target: Player
 var location_target: Vector3
 var idle_time = 0.0
+var time_in_approach = 0.0
 
 @onready var anim: AnimationPlayer = $AnimationPlayer
 @export var player_list: Array[Player]
@@ -42,12 +43,11 @@ func _physics_process(delta):
 		#my_state = state.WANDER
 		pass
 	elif my_state == state.APPROACH_PLAYER:
-		print("you stinky")
-		#if dist_to_player < some_dist:
-		#my_state = state.TEST_PLAYER
-		#if time_in_approach > some_value:
-		#my_state = state.KILL_PLAYER
-		pass
+		var dist_to_player_target = global_transform.origin.distance_to(player_target.global_transform.origin)
+		if dist_to_player_target <= PLAYER_TEST_RANGE:
+			my_state = state.TEST_PLAYER
+		if time_in_approach > APPROACH_PATIENCE_TIME:
+			my_state = state.KILL_PLAYER
 	elif my_state == state.SEEK_NOISE:
 		#move toward location that noise originated from
 		#if dist_to_player < some_value:
@@ -68,6 +68,7 @@ func _physics_process(delta):
 			if global_transform.origin.distance_to(player.global_transform.origin) < PLAYER_NOTICE_RANGE:
 				player_target = player
 				my_state = state.APPROACH_PLAYER
+				time_in_approach = 0
 	move_and_slide()
 
 func choose_wander_location():
